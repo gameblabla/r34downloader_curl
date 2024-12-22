de766e60039f65c93b0e8e1957c2b325629a6801
gameblabla <gameblabla@protonmail.com>
2024-12-22 22:46:58 +0100

    Update cli and stuff

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ I did not bother to take/convert any of the Python code and it works a bit diffe
 The initial version took me a day to program but as you can see, i also started to work on a GUI interface for video game consoles
 like the Sega Dreamcast.
 
-This one will take me more time but a POC is already working.
+This one will take me more time but a POC is already working for PC and OpenDingux.
 
 Usage
 ======
@@ -20,33 +20,57 @@ Usage
 CLI
 =====
 
-You must use a terminal or execute it from a script file. For example :
-
+You must run this program from a terminal or execute it from a script.
+Below is an example usage:
 ```
 rule34curl Sonic_the_Hedgehog f c
 ```
-Second parameter is the tag.
+- First parameter: The tag
+	Use underscores (_) instead of spaces, e.g. Sonic_the_Hedgehog or Genshin_Impact.
 
-Make sure to use _ for spaces. For example, Sonic_the_Hedgehog.
+- Second parameter: Whether you want to download full pictures or thumbnails
+	f = full/bigger pictures.
+	t = thumbnails (good for quick overviews or slow connections).
 
-3rd parameter is whenever you want to download the full pictures or the thumbnails.
+- Third parameter: The connection type
+	t = Tor background app (port 9050).
+	tb = Tor Browser proxy mode (port 9150).
+	c = clearnet (no proxy, DNS over HTTPS if compiled in).
 
-f means the bigger pictures should be downloaded.
+If you omit this parameter entirely, the downloader defaults to clearnet.
 
-t means the thumbnails should be downloaded instead.
+**Examples**
 
-Thumbnails are a good idea if you just want to have a quick overview or are behind dialup and/or slow internet connection/proxy.
+Full images on clearnet:
+```
+rule34curl Genshin_Impact f
+```
+(No third parameter means clearnet.)
 
-4th parameter is for the connection type.
+Thumbnails over Tor:
+```
+rule34curl Sonic_the_Hedgehog t t
+```
+(The final t here means Tor background on port 9050.)
 
-The "t" means you want to download it over Tor (uses port 9050), remove it if you want to download over clearnet. (without proxies)
-Use "tb" if you use the Tor browser instead. (it uses port 9150 instead)
-C means clearnet. You don't need to specify this though.
+Thumbnails over Tor Browser:
+```
+rule34curl Genshin_Impact t tb
+```
+Full images over Tor Browser:
+```
+    rule34curl Sonic_the_Hedgehog f tb
+```
 
-Since this uses CURL, it should also be able to make use of the http_proxy & https_proxy environment variables.
-If you do use that option, make sure to use the clearnet option instead.
+*Proxy Environment Variables*
 
-The images will be downloaded to the folder img.
+This program uses libcurl. If you have the environment variables http_proxy or https_proxy set, curl will try to use those.
+
+If you want to use a normal HTTP/HTTPS proxy instead of Tor, make sure to use the clearnet mode (c or omit the parameter) so it doesn’t conflict with Tor settings.
+
+**Where Are the Images Stored?**
+
+Downloaded images go into the img/ folder (created if it doesn’t exist). Thumbnails go there as well, unless you modify the code to place them elsewhere.
 
 GUI
 =====
--- src/cli.c
+++ src/cli.c
@@ -9,110 +9,169 @@
 #include "core.h"
 #include "common.h"
 
+static void print_usage(const char* program_name)
+{
+    printf("Rule34CurlC image downloader by Gameblabla\n");
+    printf("Minimal dependencies, yet somewhat unoptimized.\n\n");
+    printf("Usage: %s <tag> [thumbnail|full] [tb|tor|c]\n", program_name);
+    printf("\n");
+    printf("  <tag>         Required. Use underscores instead of spaces (e.g. Genshin_Impact).\n");
+    printf("  [thumbnail]   Download thumbnails only.\n");
+    printf("  [full]        Download full-size images (default if omitted).\n");
+    printf("\n");
+    printf("  [tb]          Use Tor Browser proxy (port 9150).\n");
+    printf("  [tor]         Use Tor background app (port 9050).\n");
+    printf("  [c]           Clearnet with DNS over HTTPS (default if omitted).\n");
+    printf("\n");
+    printf("Examples:\n");
+    printf("  %s Genshin_Impact\n", program_name);
+    printf("        -> downloads full pictures on clearnet by default.\n\n");
+    printf("  %s Genshin_Impact thumbnail c\n", program_name);
+    printf("        -> downloads thumbnails on clearnet.\n\n");
+    printf("  %s Genshin_Impact full tor\n", program_name);
+    printf("        -> downloads full images over Tor (port 9050).\n\n");
+    printf("  %s Genshin_Impact thumbnail tb\n", program_name);
+    printf("        -> downloads thumbnails over Tor Browser (port 9150).\n\n");
+    exit(1);
+}
+
 void Update_Progress(int a, int match, int all_match, char* img_filename)
 {
-	printf("Progress : %d/%d\n", a, match);	
+    printf("Progress : %d/%d\n", a, match);
 }
 
 int main(int argc, char** argv)
 {
-	long long sz;
-	char *str;
-	int i;
-	char tag_str[256];
-	char page_str[128];
-	int thumbnail_download = 0;
-	
-	printf("Rule34CurlC image downloader by Gameblabla\n");
-	printf("This is very unoptimised but it has\nminimal dependencies so it should be fairly portable.\n\nFor example, ./rule34curl Mario f c\n");
-	
-	if (argv[1] == NULL)
-	{
-		printf("No tags specified, Please specify one. Type _ rather than spaces for Tags.\n");
-		exit(1);
-	}
-	else
-	{
-		printf("Tag is %s\n", argv[1]);
-		snprintf(tag_str, sizeof(tag_str), COMMON_URL_PAGE "/post/list/%s/1", argv[1]);
-		
-		printf("Link : %s\n", tag_str);
-	}
+    long long sz;
+    char *str;
+    int i;
+    char tag_str[256];
+    char page_str[128];
+    int thumbnail_download = 0;
+    
+    if (argc < 2) {
+        print_usage(argv[0]);
+    }
 
-	if (argc > 3)
-	{
-		if (argv[3][1] == 'b' && argv[3][0] == 't')
-		{
-			printf("Tor proxy (Browser, port 9150)\n\n");
-			tor = 2;	
-		}
-		else if (argv[3][0] == 't')
-		{
-			printf("Tor proxy (port 9050)\n\n");
-			tor = 1;
-		}
-		else if (argv[3][0] == 'c')
-		{
-			printf("Clearnet (with DNS over HTTPS if support is compiled in)\n\n");
-		}
-	}
-	
-	if (argc > 2)
-	{
-		if (argv[2][0] == 't')
-		{
-			printf("Download thumbnails instead\n\n");
-			thumbnail_download = 1;
-		}
-		else if (argv[2][0] == 'f')
-		{
-			printf("Download full pictures instead\n\n");
-		}
-	}
-	
-	/* Create Folder img for storing our images and tmp for the html files */
-	create_directory(IMG_DIRECTORY, 0755);
-	create_directory(THUMB_DIRECTORY, 0755);
-	create_directory(TMP_DIRECTORY, 0755);
-	
-	/* We need to download the first page to determine how many pages are available */
-	snprintf(page_str, sizeof(page_str), TMP_DIRECTORY"/%s-page1.html", argv[1]);
-	Download_file(tag_str, page_str, tor) ;
-	sz = Get_Filesize(page_str);
-	if (sz < 5)
-	{
-		printf("sz %d, Filesize is 0!\n", sz);
-		return 1;
-	}
-	str = Read_File(page_str, sz);
+    /* 1) Parse the first argument: the tag. */
+    char* tag_arg = argv[1];
+    printf("Tag is %s\n", tag_arg);
 
-	/* From the first page, determine how many pages are available for the tag */
-	pages = Determine_Number_Pages(str, sz);
-	
-	/* If there are more than 1 page, then we will proceed to download each one of them. */
-	if (pages > 1)
-	{
-		printf("Downloading %d pages\n", pages);
-		for(i=2;i<=pages;i++)
-		{
-			printf("Progress : %d/%d\n", i, pages);
-			snprintf(tag_str, sizeof(tag_str), COMMON_URL_PAGE "/post/list/%s/%d", argv[1], i);
-			snprintf(page_str, sizeof(page_str), TMP_DIRECTORY"/%s-page%d.html", argv[1], i);
-			Download_file(tag_str, page_str, tor) ;
-		}
-	}
-	//Write_File("1.html", str, sz);
-	
-	/* If there are more than 1 page, then we will proceed to download each one of them. */
-	for(i=1;i<=pages;i++)
-	{
-		snprintf(page_str, sizeof(page_str), TMP_DIRECTORY"/%s-page%d.html", argv[1], i);
-		printf("Downloading Images in Page %d\n", i);
-		str = Read_File(page_str, sz);
-		Read_HTMLFile(str, sz, i, 0, 0, thumbnail_download, argv[1]);
-	}
-	
-	printf("Done !\nYou can find your images in the img folder !\nEnjoy fapping to them.\n");
-	
-	return 0;
+    /* 2) Default settings if not provided:
+          - thumbnail_download = 0 (full)
+          - tor = 0 (clearnet)  */
+    thumbnail_download = 0; 
+    tor = 0;
+
+    /* 3) Parse the second argument if present (thumbnail/full). */
+    if (argc >= 3) {
+        if (strcasecmp(argv[2], "thumbnail") == 0) {
+            thumbnail_download = 1;
+            printf("Download thumbnails.\n");
+        }
+        else if (strcasecmp(argv[2], "full") == 0) {
+            thumbnail_download = 0;
+            printf("Download full-size images.\n");
+        }
+        else if (strcasecmp(argv[2], "tb") == 0) {
+            /* They specified the network mode, but not the image mode -> full is default */
+            tor = 2;
+            printf("Tor Browser (port 9150)\n");
+        }
+        else if (strcasecmp(argv[2], "tor") == 0) {
+            tor = 1;
+            printf("Tor background (port 9050)\n");
+        }
+        else if (strcasecmp(argv[2], "c") == 0) {
+            tor = 0;
+            printf("Clearnet mode.\n");
+        }
+        else {
+            /* Unrecognized second argument */
+            printf("Unrecognized option: %s\n", argv[2]);
+            print_usage(argv[0]);
+        }
+    }
+
+    /* 4) Parse the third argument if present (tb, tor, c). 
+          Only if the second argument was recognized as thumbnail/full. */
+    if (argc >= 4) {
+        if (strcasecmp(argv[3], "tb") == 0) {
+            tor = 2;
+            printf("Tor Browser (port 9150)\n");
+        }
+        else if (strcasecmp(argv[3], "tor") == 0) {
+            tor = 1;
+            printf("Tor background (port 9050)\n");
+        }
+        else if (strcasecmp(argv[3], "c") == 0) {
+            tor = 0;
+            printf("Clearnet mode.\n");
+        }
+        else {
+            printf("Unrecognized option: %s\n", argv[3]);
+            print_usage(argv[0]);
+        }
+    }
+
+    printf("\nFinal settings:\n");
+    printf("  Tag: %s\n", tag_arg);
+    printf("  Mode: %s\n", thumbnail_download ? "thumbnails" : "full images");
+    if (tor == 2) {
+        printf("  Network: Tor Browser (port 9150)\n\n");
+    } else if (tor == 1) {
+        printf("  Network: Tor (port 9050)\n\n");
+    } else {
+        printf("  Network: clearnet\n\n");
+    }
+
+    /* Build the page 1 URL. */
+    snprintf(tag_str, sizeof(tag_str), "https://rule34.paheal.net/post/list/%s/1", tag_arg);
+    printf("Link : %s\n", tag_str);
+
+    /* Create directories for storing images and HTML files. */
+    create_directory(IMG_DIRECTORY, 0755);
+    create_directory(THUMB_DIRECTORY, 0755);
+    create_directory(TMP_DIRECTORY, 0755);
+    
+    /* Download the first page to figure out how many pages are available. */
+    snprintf(page_str, sizeof(page_str), TMP_DIRECTORY"/%s-page1.html", tag_arg);
+    Download_file(tag_str, page_str, tor);
+    sz = Get_Filesize(page_str);
+    str = Read_File(page_str, sz);
+
+    /* Determine how many pages are available for this tag. */
+    pages = Determine_Number_Pages(str, sz);
+
+    /* If more than 1 page, download them all. */
+    if (pages > 1)
+    {
+        printf("Downloading %d pages\n", pages);
+        for (i = 2; i <= pages; i++)
+        {
+            printf("Progress : %d/%d\n", i, pages);
+            snprintf(tag_str, sizeof(tag_str), "https://rule34.paheal.net/post/list/%s/%d", tag_arg, i);
+            snprintf(page_str, sizeof(page_str), TMP_DIRECTORY"/%s-page%d.html", tag_arg, i);
+            Download_file(tag_str, page_str, tor);
+        }
+    }
+
+    /* Parse each downloaded page and grab images. */
+    for (i = 1; i <= pages; i++)
+    {
+        snprintf(page_str, sizeof(page_str), TMP_DIRECTORY"/%s-page%d.html", tag_arg, i);
+        printf("Downloading Images in Page %d\n", i);
+        free(str); // optional: free str from the previous iteration
+        sz = Get_Filesize(page_str);
+        str = Read_File(page_str, sz);
+
+        /* Read_HTMLFile will extract links and download them (or thumbnails). */
+        Read_HTMLFile(str, sz, i, 0, 0, thumbnail_download, tag_arg);
+    }
+
+    printf("Done!\nYou can find your images in the \"%s\" folder.\n", IMG_DIRECTORY);
+    printf("Enjoy!\n");
+
+    free(str);
+    return 0;
 }
